a067e3dc712a3e4edddef4a6f3c045450b3ff76a
caishangqing <121605366@qq.com>
2018-03-30 01:01:18 +0800

    345

--- server/middleware/handler.js
+++ server/middleware/handler.js
@@ -1,3 +1,13 @@
+/**
+ * 错误事件处理
+ * 通过try catch 监听ctx.throw()发出的错误事件
+ * 参数:ctx.throw(errorCode,[message,errorPageUrl])
+ * errorCode:错误码 配置详见config中的code文件
+ * message: 自定义错误信息，如果有就是自定义如果没有则是默认配置的信息
+ * errorPageUrl：错误时重定向的地址，应为已经配置了history.js的中间件所以 没办法重定向这里解决方法是：
+ * 向前端传递一个地址 让前端路由来控制
+ */
+
 import code from "../config/code";
 
 export default app => {
@@ -12,6 +22,9 @@ export default app => {
         message: err.message || code[statusCode] || code[status] || "操作错误!",
         errorPageUrl: err.errorPageUrl || false
       };
+      // if(err.errorPageUrl){
+      //   ctx.redirect(errorPageUrl)
+      // }
     }
   };
   app.use(handler);
--- server/middleware/history.js
+++ server/middleware/history.js
@@ -1,4 +1,10 @@
-// 对connect-history-api-fallback进行封装
+/** 
+ * 对connect-history-api-fallback进行封装
+ * https://www.npmjs.com/package/connect-history-api-fallback
+ * 主要作用是支持前端route的history模式 将所有html请求重定向到index.html(插件默认页面也可以自己设置)
+ * 此插件必须放在静态资源插件的前面才可以控制
+ */
+
 import connect from 'connect-history-api-fallback';
 
 const middleware = connect({
